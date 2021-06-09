045049aa28efc38eb1d5bfb2147af0bc5486a554
Renan Loureiro <renanpl47@gmail.com>
2021-06-09 13:30:31 -0300

    create mock from storybook

---
+++ src/components/Gallery/mock.js
@@ -0,0 +1,36 @@
+export default {
+  title: 'Gallery',
+  description: 'Uma descrição sobre o conteúdo da minha galeria',
+  images: [
+    {
+      id: 1,
+      srcImg: 'https://source.unsplash.com/random/360x360?r=1',
+      text: 'foto aleatória',
+    },
+    {
+      id: 2,
+      srcImg: 'https://source.unsplash.com/random/360x360?r=2',
+      text: 'foto aleatória',
+    },
+    {
+      id: 3,
+      srcImg: 'https://source.unsplash.com/random/360x360?r=3',
+      text: 'foto aleatória',
+    },
+    {
+      id: 4,
+      srcImg: 'https://source.unsplash.com/random/360x360?r=4',
+      text: 'foto aleatória',
+    },
+    {
+      id: 5,
+      srcImg: 'https://source.unsplash.com/random/360x360?r=5',
+      text: 'foto aleatória',
+    },
+    {
+      id: 6,
+      srcImg: 'https://source.unsplash.com/random/360x360?r=6',
+      text: 'foto aleatória',
+    },
+  ],
+};
