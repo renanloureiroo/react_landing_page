a67f7f802a6609053893f675dc4103b47c97a291
Renan Loureiro <renanpl47@gmail.com>
2021-06-09 13:25:52 -0300

    create Gallery component

--- src/components/Gallery/index.jsx
+++ src/components/Gallery/index.jsx
@@ -2,11 +2,26 @@ import P from 'prop-types';
 import * as Styled from './styles';
 
 import { SectionBackground } from '../SectionBackground/index';
-export const Gallery = ({ bg = false, title, description, imgs = [] }) => {
+import { Heading } from '../Heading/index';
+import { TextComponent } from '../TextComponent/index';
+
+export const Gallery = ({ bg = false, title, description, images = [] }) => {
   return (
-    <SectionBackground>
+    <SectionBackground bg={bg}>
       <Styled.Container>
-        <Styled.TextContainer></Styled.TextContainer>
+        <Styled.TextContainer>
+          <Heading>{title}</Heading>
+          <TextComponent>{description}</TextComponent>
+        </Styled.TextContainer>
+        <Styled.GridContainer>
+          {images.map((image) => {
+            return (
+              <div key={image.id}>
+                <Styled.Grid src={image.srcImg} alt={image.text} />
+              </div>
+            );
+          })}
+        </Styled.GridContainer>
       </Styled.Container>
     </SectionBackground>
   );
@@ -16,8 +31,9 @@ Gallery.propTypes = {
   bg: P.bool,
   title: P.string.isRequired,
   description: P.string.isRequired,
-  imgs: P.arrayOf(
-    P.shap({
+  images: P.arrayOf(
+    P.shape({
+      id: P.number.isRequired,
       srcImg: P.string.isRequired,
       text: P.string.isRequired,
     }),
